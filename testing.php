e436aa9740768451aa21f8b347441756b9e6b519
KhadirYounes <146255833+KhadirYounes@users.noreply.github.com>
2023-09-30 15:57:19 +0100

    Add files via upload

---
+++ testing.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="post" action="test.php">
+      <input type="text" name="name" placeholder="your first name">
+      <input type="number" name="age" placeholder="your age">
+      <input type="text" name="msg" placeholder="your message">
+      <button type="submit" name="submit">sned</button>
+  </form>
+</body>
+</html>
